c0ed8a682e0789507d47aeee1197b16e1cf17863
Louis MILCENT <louis@lmilcent.com>
2020-01-09 10:54:53 +0100

    Allow duplicati user to run one script as root

--- duplicati/rootfs/apply-backup-group.sh
+++ duplicati/rootfs/apply-backup-group.sh
@@ -3,7 +3,7 @@
 # Apply backup group to all files and directory to backup
 # Script to call before each backup with option
 # run-script-before
-s6-setuidgid 0:0 chgrp -R duplicati-backup /backups
+sudo chgrp -R duplicati-backup /backups
 
 if [ $? -ne 0 ] ; then
   exit $?
