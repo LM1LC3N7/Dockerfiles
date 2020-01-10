c17777c84d7105a56a8760ed3582eeda5eb7d333
Louis MILCENT <louis@lmilcent.com>
2020-01-10 09:27:22 +0100

    Add chmox g+r for files and +x for directories

--- duplicati/rootfs/apply-backup-group.sh
+++ duplicati/rootfs/apply-backup-group.sh
@@ -5,8 +5,14 @@
 # run-script-before
 sudo -n chgrp -R duplicati-backup /backups
 
+# Apply rights to duplicati-backup group
+
+find /backups -type f -exec chmod g+r {} \;
+find /backups -type d -exec chmod g+rx {} \;
+
 if [ $? -ne 0 ] ; then
   exit $?
 else
   exit 0
 fi
+
